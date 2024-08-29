8676cc08c842f017e0b4575cf26fabae3b8abe2f
limoh653 <limohesbon7@gmail.com>
2024-08-29 07:50:07 +0300

    all tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+
     pass
 
 def square_integers(int_list):
+    
     # code goes here!
+    return [x**2 for x in int_list]
+
     pass
 
 def fizzbuzz():
-    # code goes here!
+    # code goes here
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
     pass
